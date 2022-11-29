c379e6b4ec7875e9ec834529a5a85314f7bddca6
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 13:56:48 -0600

    Update book.php

--- book.php
+++ book.php
@@ -4,9 +4,6 @@
   </div>
 <h1 class="text-center">Books</h1>
 <p></p>
-<p></p>
-<p></p>
-<p></p>
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card">
@@ -20,29 +17,51 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="http://prodimage.images-bn.com/pimages/9780439358071_p0_v4_s1200x630.jpg" class="card-img-top">
+                <div class="card-body">
+        <h5 class="card-title">Harry Potter: Order of the Phoenix</h5>
+        <p class="card-text">Genre: Fiction</p>
+        <p class="card-text">Price: $17.99</p>
+      </div>
+    </div>
+  </div>
+ <div class="col">
+    <div class="card">
+      <img src="https://m.media-amazon.com/images/I/41H8wZvk8VL._AC_SY780_.jpg" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">The Camel Club</h5>
+        <p class="card-text">Genre: Fiction</p>
+        <p class="card-text">Price: $8.99</p>
+      </div>
+    </div>
+  </div>
+<div class="col">
+    <div class="card">
+      <img src="https://www.davidbaldacci.com/wp-content/uploads/2019/08/9780446536141.jpg?fit=492%2C750" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">The Whole Truth</h5>
+        <p class="card-text">Genre: Fiction</p>
+        <p class="card-text">Price: $9.99</p>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="https://www.davidbaldacci.com/wp-content/uploads/2021/03/9781538719671.jpg?fit=1722%2C2600" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">A Gambling Man</h5>
+        <p class="card-text">Genre: Fiction</p>
+        <p class="card-text">Price: $8.99</p>
       </div>
     </div>
   </div>
-  <div class="col">
+ <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="https://m.media-amazon.com/images/I/51vqxnvp1iL._AC_SY350_QL15_.jpg" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">The Drop</h5>
+        <p class="card-text">Genre: Fiction</p>
+        <p class="card-text">Price: $5.99</p>
       </div>
     </div>
   </div>
-</div>
