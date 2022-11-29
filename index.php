4e3856c2a5f9178025b3c01df98f590a34caca74
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 12:27:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php require_once("header.php"); ?>
+<div class="text-center">
+  <img src="RHJ.png" class="rounded">
+</div>
+<h1>About Us</h1>
+<p>Our store is located in Norman, Oklahoma. We offer many options to buy books, while also helping you find your next book series.</p>
+<p>If you would like to call us about anything, our number is 123-456-7890</p>
+<p></p>
+<h1>This Website</h1>
+<p>The purpose of this website is to help you find a new book or find a book you would like to purchase.</p>
