93bfc6652c7ec61974ffa3a9443efda8051382d8
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 12:39:51 -0600

    Update header.php

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
           <a class="nav-link" href="publisher.php">Publishers</a>
         </li>
       </ul>
-      <form class="d-flex" class="float-end" role="search">
+      <form class="d-flex float-end" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
