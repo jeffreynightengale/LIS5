7726dfdb4c69aa759fa3438dec120671d799ded2
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 12:16:31 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LIS IA5</title>
+  </head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="book.php">Books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="branch.php">Branches</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="author.php">Authors</a>
+        </li>
+                <li class="nav-item">
+          <a class="nav-link" href="publisher.php">Publishers</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+</nav>
+<body>
