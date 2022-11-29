0918c5809c2ee570720aea4f9d9734387ccde5cd
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 13:50:05 -0600

    Update book.php

--- book.php
+++ book.php
@@ -3,6 +3,7 @@
           <img src="book2.png" class="float-start" width="120" height="60">
   </div>
 <h1 class="text-center">Books</h1>
+<p></p>
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card">
