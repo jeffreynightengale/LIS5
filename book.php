de0550557b3f96445f344668f45c10f19b222135
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 13:49:08 -0600

    Update book.php

--- book.php
+++ book.php
@@ -6,10 +6,11 @@
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="https://m.media-amazon.com/images/I/51mFoFmu0EL._AC_SY780_.jpg" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Harry Potter: Chamber of Secrets</h5>
+        <p class="card-text">Genre: Fiction</p>
+        <p class="card-text">Price: $15.99</p>
       </div>
     </div>
   </div>
