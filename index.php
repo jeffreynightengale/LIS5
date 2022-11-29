81276811836eefbfd46f3ab22343ae6a12a62379
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 12:28:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once("header.php"); ?>
 <div class="text-center">
-  <img src="RHJ.png" class="rounded">
+  <img src="book.png" class="rounded">
 </div>
 <h1>About Us</h1>
 <p>Our store is located in Norman, Oklahoma. We offer many options to buy books, while also helping you find your next book series.</p>
