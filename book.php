2886543420764cd405f69387b42281a220c15457
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-29 12:57:05 -0600

    Update book.php

--- book.php
+++ book.php
@@ -1,5 +1,5 @@
 <?php require_once("header.php"); ?>
 </div>
-          <img src="book2.png" class="float-start" width="90" height="75">
+          <img src="book2.png" class="float-start" width="120" height="60">
   </div>
 <h1 class="text-center">Books</h1>
