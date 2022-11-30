9f70d0d47feb6b5200bbb80bd9a5c73f71b814a1
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-30 15:54:02 -0600

    Update branch.php

--- branch.php
+++ branch.php
@@ -5,3 +5,31 @@
 <h1 class="text-center">Branch Information</h1>
 
 <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Barnes & Noble</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col">Book Count</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Chamber of Secrets</td>
+      <td></td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
