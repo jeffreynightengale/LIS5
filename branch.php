4127dc87af554010afd7ef3a0552560b15b27076
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-30 15:49:25 -0600

    Update branch.php

--- branch.php
+++ branch.php
@@ -1,5 +1,7 @@
 <?php require_once("header.php"); ?>
 </div>
-          <img src="book2.png" class="float-start" width="120" height="60">
+          <img src="book2.png" class="float-start" width="100" height="45">
   </div>
 <h1 class="text-center">Branch Information</h1>
+
+<table class="table table-hover">
