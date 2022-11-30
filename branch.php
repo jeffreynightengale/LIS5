5bdb3972ff2d58f3f20c17dc9c77ef63f3b0fb67
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-30 16:00:10 -0600

    Update branch.php

--- branch.php
+++ branch.php
@@ -21,15 +21,84 @@
       <td>5</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row"></th>
+      <td>Harry Potter: Order of the Phoenix</td>
+      <td></td>
+      <td>7</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <th scope="row"></th>
+      <td>The Camel Club</td>
+      <td></td>
+      <td>3</td>
+    </tr>
+        <tr>
+      <th scope="row"></th>
+      <td>The Whole Truth</td>
+      <td></td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>A Gambling Man</td>
+      <td></td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Truth</td>
+      <td></td>
+      <td>17</td>
     </tr>
   </tbody>
 </table>
+<h1></h1>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Half Price Books</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col">Book Count</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Chamber of Secrets</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Order of the Phoenix</td>
+      <td></td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Camel Club</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+        <tr>
+      <th scope="row"></th>
+      <td>The Whole Truth</td>
+      <td></td>
+      <td>13</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>A Gambling Man</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Truth</td>
+      <td></td>
+      <td>15</td>
+    </tr>
+  </tbody>
+</table>
+
