bff396d5668c334ff2838b1f4341a54a3f60dabd
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-11-30 16:03:46 -0600

    Update branch.php

--- branch.php
+++ branch.php
@@ -102,3 +102,102 @@
   </tbody>
 </table>
 
+<h1></h1>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Big Ups</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col">Book Count</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Chamber of Secrets</td>
+      <td></td>
+      <td>13</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Order of the Phoenix</td>
+      <td></td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Camel Club</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+        <tr>
+      <th scope="row"></th>
+      <td>The Whole Truth</td>
+      <td></td>
+      <td>27</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>A Gambling Man</td>
+      <td></td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Truth</td>
+      <td></td>
+      <td>4</td>
+    </tr>
+  </tbody>
+</table>
+
+<h1></h1>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Ruth's Christian</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col">Book Count</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Chamber of Secrets</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>Harry Potter: Order of the Phoenix</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Camel Club</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+        <tr>
+      <th scope="row"></th>
+      <td>The Whole Truth</td>
+      <td></td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>A Gambling Man</td>
+      <td></td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>The Truth</td>
+      <td></td>
+      <td>8</td>
+    </tr>
+  </tbody>
+</table>
